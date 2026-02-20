812561933c8f2fc4c99a5069f465e7626be8141f
Ramzy Zoldik <rabouzia@student.42.fr>
2026-02-20 17:07:14 +0100

    started from zero to implement embedded support, wifi hotsport ✅, UDP socket ✅, DNS packet parsing ✅

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,104 @@
-mod blocklist;
-mod config;
-mod handler;
+use anyhow::{bail, Context};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::{delay::FreeRtos, gpio::PinDriver, peripherals::Peripherals},
+    nvs::EspDefaultNvsPartition,
+    wifi::{
+        AccessPointConfiguration, AuthMethod, BlockingWifi, ClientConfiguration, Configuration,
+        EspWifi,
+    },
+};
+use log::info;
+use std::{collections::HashSet, net::UdpSocket};
 
-use crate::config::Config;
-use hickory_resolver::Resolver;
-use hickory_resolver::TokioResolver;
-use hickory_resolver::config::ResolverConfig;
-use hickory_resolver::name_server::TokioConnectionProvider;
-// use hickory_resolver::{TokioResolver, ResolverOpts, ResolverConfig};
-use hickory_server::ServerFuture;
-// use std::net::SocketAddr;
+const BLOCKLIST: &str = include_str!("blacklist.txt");
+const SSID: &str = "Nexus One";
+const PASSWORD: &str = "aaaaaaaa";
+const PORT: u16 = 53;
+// std::result::Result::Ok;
 
-use crate::blocklist::Blocklist;
-use crate::handler::DnsHandler;
-use std::sync::Arc;
-// use std::time::Duration;
-// use tokio::net::{TcpListener, UdpSocket};
+// fn init_wifi() -> anyhow::Result<BlockingWifi<EspWifi<'static>>> {
+//     let peripherals = Peripherals::take()?;
 
-pub struct AppState {
-    pub config: Config,
-    pub blocklist: Blocklist,
-    pub resolver: TokioResolver,
+//     let sys_loop = EspSystemEventLoop::take()?;
+//     let mut wifi = BlockingWifi::wrap(
+//         EspWifi::new(peripherals.modem, sys_loop.clone(), None)?,
+//         sys_loop,
+//     )?;
+
+//     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+//         ssid: SSID.try_into().unwrap(),
+//         password: PASSWORD.try_into().unwrap(),
+//         auth_method: AuthMethod::WPA2Personal,
+//         ..Default::default()
+//     }))?;
+
+//     wifi.start()?;
+//     wifi.connect()?;
+//     wifi.wait_netif_up()?;
+//     Ok(wifi)
+// }
+fn init_wifi() -> anyhow::Result<BlockingWifi<EspWifi<'static>>> {
+    let peripherals = Peripherals::take()?;
+    let sys_loop = EspSystemEventLoop::take()?;
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), None)?,
+        sys_loop,
+    )?;
+
+    wifi.set_configuration(&Configuration::AccessPoint(AccessPointConfiguration {
+        ssid: "esp-sinker".try_into().unwrap(),
+        password: "12345678".try_into().unwrap(),
+        auth_method: AuthMethod::WPA2Personal,
+        ssid_hidden: false,
+        ..Default::default()
+    }))?;
+
+    wifi.start()?;
+    wifi.wait_netif_up()?;
+    Ok(wifi)
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    println!("🚀 DNS Sinker - Initializing...\n");
+// fn led_debug() -> anyhow::Result<()> {
+//     let peripherals = Peripherals::take()?;
+//     let mut led = PinDriver::output(peripherals.pins.gpio2)?;
+//     println!("LED initialized, entering loop");
+//     loop {
+//         led.set_high()?;
+//         FreeRtos::delay_ms(100);
+//         led.set_low()?;
+//         FreeRtos::delay_ms(100);
+//     }
+// }
+fn main() -> anyhow::Result<()> {
+    esp_idf_svc::sys::link_patches();
+    let _nvs = EspDefaultNvsPartition::take()?;
+    esp_idf_svc::log::EspLogger::initialize_default();
+    let wifi = init_wifi().expect("error in init wifi");
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    println!("Connected with IP: {}", ip_info.ip);
 
-    let config = Config::load("config.toml")?;
-    println!("✅ Configuration loaded");
-
-    let blocklist = Blocklist::new("blocklist.txt");
-
-    let resolver = Resolver::builder_with_config(
-        ResolverConfig::default(),
-        TokioConnectionProvider::default(),
-    )
-    .build();
-    println!("✅ Upstream resolver initialized");
-
-    let app_state = Arc::new(AppState {
-        config: config,
-        blocklist,
-        resolver,
-    });
-
-    let handler = DnsHandler { state: app_state };
-
-    // 6. Create Server
-    let mut server = ServerFuture::new(handler);
-
- 
-    Ok(())
+    let socket = UdpSocket::bind(("0.0.0.0", PORT))?;
+    println!("DNS server is listening on port {}", PORT);
+    let mut buffer = [0u8; 512];
+    // led_debug().expect("error in led debug");
+    loop {
+        let (size, src) = socket.recv_from(&mut buffer)?;
+        
+        match dns_parser::Packet::parse(&buffer[..size]) {
+            Ok(packet) => {
+                for question in packet.questions{
+                    println!("Received query for: {}", question.qname);
+                }
+            }
+            Err(e) => {
+                println!("Failed to parse DNS packet: {}", e);
+                continue;
+            }
+        }
+        
+        println!("Received {} bytes from {}", size, src);
+        // std::thread::sleep(core::time::Duration::from_secs(5));
+    }
+    // Ok(())
 }
